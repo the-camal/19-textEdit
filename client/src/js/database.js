8236c65883322ac4974c398fe6c03f19d3d7c344
paulcourts <hubertcourts@gmail.com>
2023-01-25 16:04:12 -0600

    getting the data dase done

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -11,11 +11,36 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
+  export const getOneDb = async (id) => {
+    console.log('GET from the database');
+    const todosDb = await openDB('todos', 1);
+    const tx = todosDb.transaction('todos', 'readonly');
+    const store = tx.objectStore('todos');
+    const request = store.get(id);
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+    };
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const deleteDb = async (id) => {
+  console.log('DELETE from the database', id);
+  const todosDb = await openDB('todos', 1);
+  const tx = todosDb.transaction('todos', 'readwrite');
+  const store = tx.objectStore('todos');
+  const request = store.delete(id);
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (id, content) => {
+  console.log('PUT to the database');
+  const todosDb = await openDB('todos', 1);
+  const tx = todosDb.transaction('todos', 'readwrite');
+  const store = tx.objectStore('todos');
+  const request = store.put({ id: id, todo: content });
+  const result = await request;
+  console.log('data saved to the database', result);
+};
 
 initdb();
